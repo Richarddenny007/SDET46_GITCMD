23a02f7f4286d779e767a927313b9af8c6029483
Richarddenny007 <rakshithdenny@gmail.com>
2023-03-13 13:10:10 +0530

    icc

--- src/test/java/Practice/ICCcricket.java
+++ src/test/java/Practice/ICCcricket.java
@@ -19,24 +19,14 @@ public class ICCcricket {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.icc-cricket.com/homepage");
-		WebElement ranking = driver.findElement(By.xpath("(//button[normalize-space(text())='Rankings'])[1]"));
 		
-		Generic_methods gen=new Generic_methods();
-		gen.initializeAction(driver);
-		gen.moveToElement(ranking);
+		
+		
+		
 		
 		
 		
 	
-		Thread.sleep(2000);
-		String rank="Test Team Rankings";
-		String team="Australia";
-		driver.findElement(By.xpath("(//a[normalize-space(text())='Overview' and @href='/rankings/mens/overview' ])[1]")).click();
-      WebElement rating = driver.findElement(By.xpath("//h4[.='"+rank+"']/ancestor::div[@class='rankings-block__container rankings-block__container--teams']"
-      		+ "//div[.='"+team+"']"
-       + "/following-sibling::div[@class='rankings-block__banner--rating u-text-right']"));
-      String text = rating.getText();
-      System.out.println(text);
 	}
 
 }
